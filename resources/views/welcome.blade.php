42faf4e6f40187129763ee8830aae8bade8bcded
Lance Ruzel <134846182+lanceruzel@users.noreply.github.com>
2024-06-17 22:48:08 +0800

    Project Setup

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,14 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
-
+        <wireui:scripts />
+        <script src="//unpkg.com/alpinejs" defer></script>
+        @vite(['resources/css/app.css','resources/js/app.js'])
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+        
 
         <!-- Styles -->
         <style>
@@ -164,6 +168,8 @@
 
                     <footer class="py-16 text-center text-sm text-black dark:text-white/70">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+
+                        <x-alert title="Any Message!" />
                     </footer>
                 </div>
             </div>
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  presets: [
+    require("./vendor/wireui/wireui/tailwind.config.js")
+  ],
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/**/*.blade.php", 
+    "./resources/**/**/**/*.blade.php", 
+    "./resources/views/components/**/*.blade.php", 
+    "./resources/views/livewire/**/*.blade.php", 
+    "./vendor/wireui/wireui/src/*.php",
+    "./vendor/wireui/wireui/ts/**/*.ts",
+    "./vendor/wireui/wireui/src/WireUi/**/*.php",
+    "./vendor/wireui/wireui/src/Components/**/*.php",
+  ],
+  theme: {
+    extend: {
+      fontFamily:{
+        inter:['Inter', 'system-ui', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
