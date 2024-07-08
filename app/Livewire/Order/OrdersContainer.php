cb2d2ba7dd6f33b8758ef48e9cd9579f11f06122
Lance Ruzel <134846182+lanceruzel@users.noreply.github.com>
2024-07-08 22:16:17 +0800

    Setup orders page

---
+++ app/Livewire/Order/OrdersContainer.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Order;
+
+use Livewire\Component;
+
+class OrdersContainer extends Component
+{
+    public function render()
+    {
+        return view('livewire.Order.orders-container');
+    }
+}
--- resources/views/livewire/Pages/orders.blade.php
+++ resources/views/livewire/Pages/orders.blade.php
@@ -1,13 +1,13 @@
 <x-layouts.main-layout>
     <div class="w-full max-w-[1000px] mx-auto px-3 h-full">
         <div class="pb-2.5">
-            <div class="flex max-sm:items-start items-center max-sm:flex-col justify-between gap-4">
-                <p class="max-md:text-2xl text-4xl font-semibold">Orders</p>
-            </div>
+            
+            <p class="max-md:text-2xl text-4xl font-semibold">Orders</p>
+            
 
             <!-- content -->
         </div>
     </div>
 
-    {{-- <livewire:Ordering.cart-total-checkout /> --}}
+    <livewire:Order.orders-container />
 </x-layouts.main-layout>
