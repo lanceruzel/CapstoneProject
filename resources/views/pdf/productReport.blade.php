e6df46bc190c89420adf62935a896ee76ac781d4
LanceRuzel <lanceruzel2@gmail.com>
2024-10-01 23:13:03 +0800

    update export details

--- resources/views/pdf/productReport.blade.php
+++ resources/views/pdf/productReport.blade.php
@@ -50,11 +50,16 @@
     <div class="section">
         <h2>Product Information</h2>
         <div class="info">
-            <p><strong>Product Name: </strong>{{ $report->product->name }}</p>
-            <p><strong>Product ID: </strong>#{{ $report->product->id }}</p>
-            <p><strong>Seller Name: </strong>{{ $report->product->seller->name() }}</p>
-            <p><strong>Seller ID: </strong>#{{ $report->product->seller->id }}</p>
-            <p><strong>Listing Date: </strong>{{ date('M d, Y', strtotime($report->product->created_at)) }}</p>
+            <p><strong>Products: </strong>
+                @foreach(json_decode($report->products) as $items)
+                    <p>{{ '(#'. $items->id .') ' . $items->name }}
+                        @if(!$loop->last),@endif
+                    </p>
+                @endforeach
+            </p>
+            <p></p>
+            <p><strong>Seller Name: </strong>{{ $report->seller->name() }}</p>
+            <p><strong>Seller ID: </strong>#{{ $report->seller->id }}</p>
         </div>
     </div>
 
@@ -64,9 +69,10 @@
         <div class="info">
             <p><strong>Report ID: </strong>#{{ $report->id }}</p>
             <p><strong>Report Date: </strong>{{ date('M d, Y', strtotime($report->created_at)) }}</p>
-            <p><strong>User: </strong>{{ $report->user->name() }}</p>
-            <p><strong>User ID: </strong>#{{ $report->user->id }}</p>
-            <p><strong>Complaint Description: </strong>"{{ $report->content }}"</p>
+            <p><strong>User: </strong>{{ $report->reporter->name() }}</p>
+            <p><strong>User ID: </strong>#{{ $report->reporter->id }}</p>
+            <p><strong>Reason: </strong>{{ $report->reason }}</p>
+            <p><strong>Complaint Description: </strong>{{ $report->description }}</p>
             {{-- <p><strong>Action Taken: </strong>Product Suspended</p> --}}
         </div>
     </div>
