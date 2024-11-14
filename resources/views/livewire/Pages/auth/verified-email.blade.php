211dcc0d03f11a9a55d236a4b9df417bc6122e90
LanceRuzel <lanceruzel2@gmail.com>
2024-11-14 19:58:54 +0800

    adjust email verification message on verify

--- resources/views/livewire/Pages/auth/verified-email.blade.php
+++ resources/views/livewire/Pages/auth/verified-email.blade.php
@@ -17,16 +17,18 @@
         <div class="overflow-hidden h-full">
             <div class="relative flex items-center justify-center h-full w-full px-3 md:px-5 lg:px-10 z-60">
                 <div class="flex max-w-[500px] flex-col items-center justify-center max-lg:mt-40 text-center bg-gray-50 p-12 rounded shadow">
-                    <h2 class="text-xl font-bold text-green-600 mb-4">Congratulations! 🎉</h2>
+
+                <h2 class="text-xl font-bold text-green-600 mb-4">Congratulations! 🎉</h2>
+            
                 <p class="text-gray-700 mb-4">
                     Your email verification was successful, and your account is now fully activated. You’re all set to explore all the features and benefits we have in store for you!
                 </p>
-
+                
                 <p class="text-gray-700 mb-6">
-                    To proceed, simply click the button below:
+                    To proceed, you will be redirected to the login page. Click the button below to continue:
                 </p>
                 
-                <x-button class="mt-3 w-full" label="Proceed" href="{{ route('home') }}" />
+                <x-button class="mt-3 w-full" label="Proceed" href="{{ route('signout') }}" />
 
                 <div class="absolute top-0 w-full h-full max-w-[1500px] z-[-10] object-auto">
                     <img src="{{ asset('assets/svg/13124789_5156365.svg') }}" alt="Traveling-cuate"/>
